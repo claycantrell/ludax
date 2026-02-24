7ee7d0a2e7ceff534456ed6bcfc39cd5b5cb9409
Padlex <alexpadula2001@gmail.com>
2026-02-24 12:21:10 +0100

    Restore compatibility with python 3.9+

--- src/ludax/utils.py
+++ src/ludax/utils.py
@@ -388,13 +388,13 @@ def _get_mask_board_conversion_fns(game_info: GameInfo):
         base_board = jnp.ones_like(base, dtype=jnp.int8) * INVALID
 
         def mask_to_board(mask):
-            return base_board.at[*valid_hex_indices].set(mask)
+            return base_board.at[tuple(valid_hex_indices)].set(mask)
         
         def mask_idx_to_board_pos(idx):
             return valid_hex_indices.T[idx]
         
         def board_to_mask(board):
-            return board[*valid_hex_indices].flatten()
+            return board[tuple(valid_hex_indices)].flatten()
         
         return mask_to_board, mask_idx_to_board_pos, board_to_mask
 
