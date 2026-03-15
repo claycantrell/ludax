5d8ceeb30709518dd17d850e3e1b39c32970cba0
Padlex <alexpadula2001@gmail.com>
2026-03-15 13:20:09 +0100

    Minor cleanup

--- examples/02-ludax_gui/interactive.py
+++ examples/02-ludax_gui/interactive.py
@@ -1,5 +1,10 @@
+import argparse
 from ludax.gui import app
 
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('--port', type=int, default=8080)
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=True)
